39fcfa4b7c7b933f743609d96be76e913736a2d4
Katrina Wilson <kwilson@unc.edu>
2023-10-09 02:17:21 -0400

    embed header and footer

--- public_html/itis3135/scripts/defaultscripts.js
+++ public_html/itis3135/scripts/defaultscripts.js
@@ -13,13 +13,27 @@ function printTime() {
 }
 
 window.getMood = function(form) {
-
     var fname = form.fname.value;
     var mood = form.mood.value;
     document.write("Wilson Enterprises welcome you, "+ fname + ". We are glad you are doing "+ mood +"!");
     
 }
 
+function updateGreeting() {
+    var fname = document.getElementById("fname").value;
+    var mood = document.getElementById("mood").value;
+    
+    greeting = "Wilson Enterprises welcome you, " + fname + ". We are glad you are feeling " + mood + "!";
+}
 
+function update(){
+    var fname = document.getElementById("fname").value;
+    var mood = document.getElementById("mood").value;
+    document.getElementById("mood").value = "chan";
+    document.write("Wilson Enterprises welcome you, "+ fname + ". We are glad you are doing "+ mood +"!");
 
+}
 
+let age = prompt('How old are you?', 100);
+
+alert(`You are ${age} years old!`); // You are 100 years old!
