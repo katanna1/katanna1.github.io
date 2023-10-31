cfdc13da2d6ba272f2d7e499434178c07f1ba016
Katrina Wilson <kwilson@unc.edu>
2023-10-31 13:31:07 -0400

    Update calculator_try.js

--- public_html/itis3135/scripts/calculator_try.js
+++ public_html/itis3135/scripts/calculator_try.js
@@ -3,43 +3,74 @@ const keys= calculator.querySelector('.calculator_keys');
 
 keys.addEventListener('click', e => {
     if (e.target.matches('button')){
-        alert(1);
-    }
-})
-
-const key = e.target;
-const action = key.dataset.action;
-
-if (!action) {
-    console.log('number key');
-}
-
-if (
-    action === 'add' ||
-    action === 'subtract' ||
-    action === 'multiply' ||
-    action === 'divide'
-) {
-    console.log('operator key');
-}
-
-if (action === 'decimal') {
-    console.log('decimal key');
-}
-
-if (action === 'clear') {
-    console.log('clear key');
-}
-
-if (action === 'calculate') {
-    console.log('equal key');
-}
-
-const display = document.querySelector('.calculator_display');
-
-keys.addEventListener('click', e => {
-    if (e.target.matches('button')) {
+                
         const key = e.target;
-        const ac
+        const action = key.dataset.action;
+
+        if (!action) {
+            console.log('number key');
+        }
+
+        if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+        ) {
+            console.log('operator key');
+        }
+
+        if (action === 'decimal') {
+            console.log('decimal key');
+        }
+
+        if (action === 'clear') {
+            console.log('clear key');
+        }
+
+        if (action === 'calculate') {
+            console.log('equal key');
+        }
+
+        const display = document.querySelector('.calculator_display');
+
+        keys.addEventListener('click', e => {
+            if (e.target.matches('button')) {
+                const key = e.target;
+                const action = key.dataset.action;
+                const keyContent = key.textContent;
+                const displayedNum = display.textContent;
+            }
+        })
+
+        if(!action) {
+            if(displayNum === '0') {
+                display.textContent = keyContent;
+            }
+        }
+
+        if (!action) {
+            if (displayedNum === '0') {
+                display.textContent = keyContent;
+            } else {
+                display.textContent = displayedNum + keyContent;
+            }
+        }
+
+        if (action === 'decimal') {
+            display.textContent = displayedNum + '.';
+        }
+
+        if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+        ) {
+            key.classList.add('is-depressed');
+        }
+
+        Array.from(key.parentNode.children)
+            .forEach (k => k.class.remove('is-depressed'))
     }
 })
